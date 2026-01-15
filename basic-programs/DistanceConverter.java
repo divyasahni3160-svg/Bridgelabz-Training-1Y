f6cda1c92859c1238ec8b85aa536df51f897c5c2
divyasahni3160-svg <divyasahni3160@gmail.com>
2026-01-15 23:09:13 +0530

    2 assignment q12

---
+++ basic-programs/DistanceConverter.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class DistanceConverter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input: distance in feet
+        System.out.print("Enter the distance in feet: ");
+        double distanceInFeet = sc.nextDouble();
+
+        // Conversion
+        double distanceInYards = distanceInFeet / 3.0;   // 1 yard = 3 feet
+        double distanceInMiles = distanceInYards / 1760.0; // 1 mile = 1760 yards
+
+        // Output
+        System.out.println("The distance is " + distanceInYards + " yards and " + distanceInMiles + " miles.");
+
+        sc.close();
+    }
+}
