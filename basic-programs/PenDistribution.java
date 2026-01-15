97543401de4591806772d5689042a42d2d23c70d
divyasahni3160-svg <divyasahni3160@gmail.com>
2026-01-15 21:58:40 +0530

    second assignment q5

---
+++ basic-programs/PenDistribution.java
@@ -0,0 +1,9 @@
+public class PenDistribution{
+public static void main(String[] args){
+int p = 14;
+int s = 3;
+int d = p/3;
+int r = p%3;
+System.out.println("The Pen Per Student is " + d + " and the remaining pen not distributed is " + r);
+}
+}
