0a52eeaba64033e4f3a14eff6e51a84ab2017ae5
divyasahni3160-svg <divyasahni3160@gmail.com>
2026-01-15 20:37:31 +0530

    second assignment

---
+++ basic-programs/harryage.java
@@ -0,0 +1,9 @@
+public class harryage{
+public static void main(String[] args){
+String a = "harry's";
+int b = 2000;
+int c = 2024;
+System.out.println(a + " age in " + c + " is " + (c-b));
+}
+}
+
