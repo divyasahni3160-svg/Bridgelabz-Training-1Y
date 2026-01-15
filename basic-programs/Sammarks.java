5ebf09dd24d2de148537be9cebfdf62c7f303995
divyasahni3160-svg <divyasahni3160@gmail.com>
2026-01-15 21:01:03 +0530

    second assignment q2

---
+++ basic-programs/Sammarks.java
@@ -0,0 +1,10 @@
+public class Sammarks{
+public static void main(String[] args){
+String a = "Sam's";
+double m = 94;
+double p = 95;
+double c = 96;
+double av = (p+c+m)/3;
+System.out.println(a + " average marks in pcm is " + av);
+}
+}
