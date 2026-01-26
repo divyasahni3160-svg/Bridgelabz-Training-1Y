a03e15152f9a86a9178ea20da0aaadae385c1f2e
divyasahni3160-svg <divyasahni3160@gmail.com>
2026-01-26 09:04:51 +0530

    [Divya]Add.NoDivisibleBy5

---
+++ basic-programs/NoDivisibleBy5.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class NoDivisibleBy5{
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+int x = sc.nextInt();
+int y;
+if((x%5) == 0){
+System.out.println("Is the number " + x + " divisible by 5? Yes");
+}else{
+System.out.println("Is the number " + x + " divisible by 5? no");}
+}
+}
