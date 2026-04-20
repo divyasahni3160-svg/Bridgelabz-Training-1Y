4865bdf94307389de3a2f3ee7d840ae78b8a366b
divyasahni3160-svg <divyasahni3160@gmail.com>
2026-04-21 00:12:53 +0530

    a

---
+++ assingment/regex/Pass.java
@@ -0,0 +1,18 @@
+package com.gla.AA;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Pass {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String regex="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&*!?])[A-Za-z\\d@$!%*?&]{8,16}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher=pattern.matcher("Ab@5asfg64546");
+        if(matcher.matches())
+            System.out.println("valid");
+        else
+            System.out.println("invalid");
+    }
+}
